05f828bcf491b965dc00c6bbd81cb9028a6831f5
hong9Lee <hongtemp9mac@gmail.com>
2024-12-08 20:19:14 +0900

    init

---
+++ src/main/kotlin/com/example/log_save_architecture/LogSaveArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package com.example.log_save_architecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogSaveArchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogSaveArchitectureApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/log_save_architecture/LogSaveArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.log_save_architecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LogSaveArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
